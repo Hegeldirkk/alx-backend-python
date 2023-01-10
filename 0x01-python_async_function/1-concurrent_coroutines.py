da1431d339870159840d80f1f6c443455d3fce7f
Hegeldirkk <hegeldirkk@gmail.com>
2023-01-10 04:44:36 +0000

    script git writting - ojkkk

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """return the list of all the delays
-    (float values) list of the delays should be in
-    ascending order without using sort()"""
-    wait_list = []
-    result = []
+    """return the list of all the del"""
+    wait_list, result = [], []
 
     for _ in range(n):
         wait_list.append(wait_random(max_delay))
