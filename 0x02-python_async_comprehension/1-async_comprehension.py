8c7151c707043f1f3eabd4d00bd0433340c47a56
Hegeldirkk <hegeldirkk@gmail.com>
2023-01-10 16:19:42 +0000

    script git writting - async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Async Comprehensions"""
+
+
+import asyncio
+from typing import List, Generator
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[None, None, List[float]]:
+    """collect 10 random numbers using
+    an async comprehensing return the 10
+    random numbers"""
+    result = []
+    result = [i async for i in async_generator()]
+    return result
